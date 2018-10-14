60f96947bc10e3d894b6c93dcaf4e24bc98970e9
mis0b1gboy <37484559+mis0b1gboy@users.noreply.github.com>
2018-10-14 18:39:44 -0500

    Update tor_service.sh

--- tor_service.sh
+++ tor_service.sh
@@ -2,17 +2,13 @@
 
 cd ~/ 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential pkg-config sqlite3 openssl zlibc zlib1g zlib1g-dev make  -y
-wget https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.50.tar.gz
-tar xzvf lighttpd-1.4.50.tar.gz
-cd ~/lighttpd-1.4.50
-echo './configure \' > config.sh
-echo '"--prefix=$HOME/light" \' >> config.sh
-echo '"--disable-ipv6" \' >> config.sh
-echo '"--without-pcre" \' >> config.sh
-echo '"--without-zlib" \' >> config.sh
-echo '"--without-bzip2"' >> config.sh
-sh config.sh
-make
-make install
-mv ~/tor_service/lighttpd.conf ~/light/
+sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential libevent-dev tor openssl lighttpd zlib1g-dev libssl-dev  -y
+cp lighttpd.conf /etc/lighttpd
+sudo lighttpd-enable-mod userdir
+sudo service lighttpd reload
+mkdir /home/ubuntu/hidden_service
+mkdir /home/ubuntu/other_hidden_service
+sudo cp torrc /etc/tor
+chmod 700 /home/ubuntu/hidden_service
+chmod 700 /home/ubuntu/other_hidden_service
+tor
