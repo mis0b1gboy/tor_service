d2337c1e650a4977ab4536002b9474bed199c0a2
mis0b1gboy <37484559+mis0b1gboy@users.noreply.github.com>
2018-10-14 16:36:32 -0500

    Add files via upload

---
+++ tor_service.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~/ 
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install make -y
+wget https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.50.tar.gz
+tar xzvf lighttpd-1.4.50.tar.gz
+cd ~/lighttpd-1.4.50
+echo './configure \' > config.sh
+echo '"--prefix=$HOME/light" \' >> config.sh
+echo '"--disable-ipv6" \' >> config.sh
+echo '"--without-pcre" \' >> config.sh
+echo '"--with-zlib" \' >> config.sh
+echo '"--without-bzip2"' >> config.sh
+sh config.sh
+make
+make install
+mv lighttpd.conf ~/light/
