30055445e25631a9d048d6895e1d0dfabefe5c8d
mis0b1gboy <37484559+mis0b1gboy@users.noreply.github.com>
2018-10-14 17:19:29 -0500

    Update tor_service.sh

--- tor_service.sh
+++ tor_service.sh
@@ -2,7 +2,7 @@
 
 cd ~/ 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential make -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential pkg-config sqlite3 openssl zlibc zlib1g zlib1g-dev make  -y
 wget https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.50.tar.gz
 tar xzvf lighttpd-1.4.50.tar.gz
 cd ~/lighttpd-1.4.50
@@ -10,7 +10,7 @@ echo './configure \' > config.sh
 echo '"--prefix=$HOME/light" \' >> config.sh
 echo '"--disable-ipv6" \' >> config.sh
 echo '"--without-pcre" \' >> config.sh
-echo '"--with-zlib" \' >> config.sh
+echo '"--without-zlib" \' >> config.sh
 echo '"--without-bzip2"' >> config.sh
 sh config.sh
 make
