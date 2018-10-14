de8b9e476af5ff65632216d8537d9eec1cd4d9e5
mis0b1gboy <37484559+mis0b1gboy@users.noreply.github.com>
2018-10-14 16:41:20 -0500

    Update tor_service.sh

--- tor_service.sh
+++ tor_service.sh
@@ -2,7 +2,7 @@
 
 cd ~/ 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install make -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install build-essential make -y
 wget https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.50.tar.gz
 tar xzvf lighttpd-1.4.50.tar.gz
 cd ~/lighttpd-1.4.50
@@ -16,4 +16,4 @@ sh config.sh
 make
 make install
 mkdir ~/light
-mv lighttpd.conf ~/light/
+mv ~/tor_service/lighttpd.conf ~/light/
